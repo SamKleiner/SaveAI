8e376aa2f592d93af259cc58923bfc99fa33e6e5
Sam Kleiner <sk03row@gmail.com>
2025-03-01 17:09:48 -0500

    Initial Commit

---
+++ SaveAI/SaveAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveAIApp.swift
+//  SaveAI
+//
+//  Created by Sam Kleiner on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SaveAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SaveAITests/SaveAITests.swift
@@ -0,0 +1,17 @@
+//
+//  SaveAITests.swift
+//  SaveAITests
+//
+//  Created by Sam Kleiner on 3/1/25.
+//
+
+import Testing
+@testable import SaveAI
+
+struct SaveAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
